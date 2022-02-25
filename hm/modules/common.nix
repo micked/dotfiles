8c0710027fc907b5232dbf4d2d910e5f8031da76
Michael Schantz Klausen <sch@ntz.nu>
2022-02-25 16:18:18 +0100

    Added c2_pass and pass in general

--- hm/modules/common.nix
+++ hm/modules/common.nix
@@ -1,14 +1,19 @@
 { config, pkgs, libs, ... }:
 
 let 
-  fx = pkgs.writeShellScriptBin "fx" "firefox $@";
-  calc = pkgs.writeShellScriptBin "calc" "gnome-calculator $@";
+  fx = pkgs.writeShellScriptBin "fx" "${pkgs.firefox}/bin/firefox $@";
+  calc = pkgs.writeShellScriptBin "calc" "${pkgs.gnome.gnome-calculator}/bin/gnome-calculator $@";
+  c2_pass = pkgs.writeShellScriptBin "c2_pass" ''
+    ${pkgs.pass}/bin/pass c2
+    ${pkgs.oathToolkit}/bin/oathtool -v --totp=sha256 --digits=8 $(pass c2_secret) | tail -n1
+  '';
 
 in {
   home.packages = with pkgs; [
     # my stuff
     fx
     calc
+    c2_pass
 
     # other
     sublime4
@@ -20,6 +25,7 @@ in {
     inkscape
     teams
     gnome.gnome-calculator
+    pass
 
     meslo-lgs-nf
   ];
@@ -47,5 +53,14 @@ in {
     templates = "$HOME/templates";
     videos = "$HOME";
     createDirectories = true;
+  };
+
+  services.gpg-agent = {
+    enable = true;
+    extraConfig = ''
+      default-cache-ttl 36000
+      max-cache-ttl 36000
+    '';
+    pinentryFlavor = "tty";
   };
 }
