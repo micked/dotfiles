430b70706b91f4fd5537f2dd2109a270d302a341
Michael Schantz Klausen <sch@ntz.nu>
2022-03-09 14:38:32 +0100

    Added zotero

--- hm/modules/work.nix
+++ hm/modules/work.nix
@@ -1,6 +1,10 @@
 { config, pkgs, libs, ... }:
 {
 
+  home.packages = with pkgs; [
+    zotero
+  ];
+
   services.dropbox.enable = true;
   services.dropbox.path = "${config.home.homeDirectory}/dropbox";
 
