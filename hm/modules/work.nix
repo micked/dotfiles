4e42bf98259cbe1a4925c47a0db5127bfab8446e
Michael Schantz Klausen <sch@ntz.nu>
2022-03-15 10:15:28 +0100

    Added CLC hack

--- hm/modules/work.nix
+++ hm/modules/work.nix
@@ -1,9 +1,16 @@
 { config, pkgs, libs, ... }:
-{
+
+let
+  clc = pkgs.writeShellScriptBin "clc" "/home/msk/keep/clc21/clcmainwb21";
+
+in {
 
   home.packages = with pkgs; [
     zotero
     logseq
+
+    clc
+    jdk11 #For CLC
   ];
 
   services.dropbox.enable = true;
