323b8ace2a9bd2b9987bdf0fd6035b2c3e2d9c9e
Michael Schantz Klausen <sch@ntz.nu>
2022-06-04 18:27:10 +0200

    Fixed svg2shenzhen

--- modules/inkscape.nix
+++ modules/inkscape.nix
@@ -1,15 +1,30 @@
 { config, pkgs, libs, ... }:
 
 let
-  inkscape-svg2shenzhen = pkgs.fetchzip {
-    url = "https://github.com/badgeek/svg2shenzhen/releases/download/0.2.18.7/svg2shenzhen-extension-0.2.18.7.tar.gz";
-    sha256 = "KN17dt8X8UqA2zClxICzLyMkMQ8hWuktznMqkuBFGdc=";
-    stripRoot=false;
+  inkscape-svg2shenzhen = pkgs.stdenv.mkDerivation {
+    version = "0.2.18.7";
+    pname = "inkscape-svg2shenzhen";
+    src = pkgs.fetchFromGitHub {
+      owner = "badgeek";
+      repo = "svg2shenzhen";
+      rev = "0.2.18.7";
+      sha256 = "48U0JgF81C5nQ2qDAM3oM+6oGS4ZavxIsuEI7OnF9bM=";
+    };
+
+    nativeBuildInputs = [
+      pkgs.boost169
+    ];
+
+    installPhase = ''
+      mkdir -p $out
+      cp -r inkscape/* $out/
+      cp bitmap2component $out/svg2shenzhen/bitmap2component_linux64
+    '';
   };
 in {
   home.packages = with pkgs; [
     inkscape
-    inkscape-svg2shenzhen
+    cantarell-fonts
   ];
 
   home.file = {
