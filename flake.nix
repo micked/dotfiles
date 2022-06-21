996d11c39e71446a2b87a601fad98b56c9e4e55b
Michael Schantz Klausen <sch@ntz.nu>
2022-06-21 15:47:50 +0200

    Simplify flake a little

--- flake.nix
+++ flake.nix
@@ -5,9 +5,14 @@
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
   outputs = {self, nixpkgs, home-manager, ... }@inputs:
-  {
-
-    # --
+  let
+    homeAtWork = { config, pkgs, ... }: {
+      home.homeDirectory = "/home/msk";
+      home.username = "msk";
+      home.stateVersion = "22.05";
+      imports = [ ./modules/common.nix ./modules/work.nix ];
+    };
+  in {
 
     nixosConfigurations.principle = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
@@ -31,16 +36,7 @@
         home-manager.nixosModules.home-manager {
           home-manager.useGlobalPkgs = true;
           home-manager.useUserPackages = true;
-          home-manager.users.msk = { config, pkgs, ... }:
-          {
-            home.homeDirectory = "/home/msk";
-            home.username = "msk";
-            home.stateVersion = "22.05";
-            imports = [
-              ./modules/common.nix
-              ./modules/work.nix
-            ];
-          };
+          home-manager.users.msk = homeAtWork;
         }
       ];
     };
@@ -68,29 +64,6 @@
             # ./modules/zsh.nix
             ./modules/syncthing.nix
             # ./modules/awesomewm.nix
-          ];
-        };
-      };
-
-      work = inputs.home-manager.lib.homeManagerConfiguration {
-        system = "x86_64-linux";
-        homeDirectory = "/home/msk";
-        username = "msk";
-        stateVersion = "21.11";
-        configuration = { config, lib, pkgs, ... }:
-        {
-          # nixpkgs.config = { allowUnfree = true; };
-          nixpkgs.config.allowUnfreePredicate = (_: true);
-          programs.home-manager.enable = true;
-
-          imports = [
-            ./modules/common.nix
-            ./modules/work.nix
-            # ./modules/git.nix
-            # ./modules/vim.nix
-            # ./modules/zsh.nix
-            # ./modules/awesomewm.nix
-            # ./modules/vscode.nix
           ];
         };
       };
