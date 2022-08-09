e09d0b1cce8e7aace650a7c8c869579ac8e4c61b
Michael Klausen <msk@evaxion-biotech.com>
2022-08-09 14:21:18 +0200

    Added beginning of compute home-manager config

--- modules/vim.nix
+++ modules/vim.nix
@@ -25,7 +25,13 @@ in {
       }
       {
         plugin = nvim-base16;
-        config = "colorscheme base16-gruvbox-dark-medium";
+        config = ''
+          if filereadable(expand("~/.vimrc_background"))
+            let base16colorspace=256
+            source ~/.vimrc_background
+          else
+            colorscheme base16-gruvbox-dark-medium";
+          endif'';
       }
     ];
 
