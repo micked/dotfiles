a7995c6788ebb6908daeea75d3db7173205fc77b
Michael Schantz Klausen <msk@evaxion-biotech.com>
2022-08-10 09:49:59 +0200

    Added conda loader to evxcompute

--- hm-evxcompute.nix
+++ hm-evxcompute.nix
@@ -4,6 +4,29 @@
   home.username = "msk";
   home.homeDirectory = "/home/msk";
 
+  home.file.".hushlogin".source = pkgs.writeTextFile {
+    name = "hushlogin";
+    text = "";
+  };
+  home.file.".config/conda.zsh".source = pkgs.writeTextFile {
+    name = "conda.zsh";
+    text = ''# >>> conda initialize >>>
+      # !! Contents within this block are managed by 'conda init' !!
+      __conda_setup="$('/work/msk/miniconda3/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
+      if [ $? -eq 0 ]; then
+          eval "$__conda_setup"
+      else
+          if [ -f "/work/msk/miniconda3/etc/profile.d/conda.sh" ]; then
+              . "/work/msk/miniconda3/etc/profile.d/conda.sh"
+          else
+              export PATH="/work/msk/miniconda3/bin:$PATH"
+          fi
+      fi
+      unset __conda_setup
+      # <<< conda initialize <<<
+    '';
+  };
+
   imports = [
     ./modules/git.nix
     ./modules/vim.nix
