fb6e59910c9eafae46d28f96f463170463e642c0
Michael Schantz Klausen <msk@evaxion-biotech.com>
2022-09-12 12:28:12 +0200

    Add micromamba-zsh

--- hm-evxcompute.nix
+++ hm-evxcompute.nix
@@ -1,24 +1,5 @@
 { config, pkgs, ... }:
 let
-  conda-zsh = pkgs.writeTextFile {
-    name = "conda.zsh";
-    text = ''
-      # >>> conda initialize >>>
-      # !! Contents within this block are managed by 'conda init' !!
-      __conda_setup="$('/work/msk/miniconda3/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
-      if [ $? -eq 0 ]; then
-          eval "$__conda_setup"
-      else
-          if [ -f "/work/msk/miniconda3/etc/profile.d/conda.sh" ]; then
-              . "/work/msk/miniconda3/etc/profile.d/conda.sh"
-          else
-              export PATH="/work/msk/miniconda3/bin:$PATH"
-          fi
-      fi
-      unset __conda_setup
-      # <<< conda initialize <<<
-    '';
-  };
   micromamba-bash = pkgs.writeTextFile {
     name = "micromamba.bash";
     text = ''
@@ -27,6 +8,27 @@ let
       export MAMBA_EXE="${pkgs.micromamba}/bin/micromamba";
       export MAMBA_ROOT_PREFIX="/work/msk/micromamba";
       __mamba_setup="$('${pkgs.micromamba}/bin/micromamba' shell hook --shell bash --prefix '/work/msk/micromamba' 2> /dev/null)"
+      if [ $? -eq 0 ]; then
+          eval "$__mamba_setup"
+      else
+          if [ -f "/work/msk/micromamba/etc/profile.d/micromamba.sh" ]; then
+              . "/work/msk/micromamba/etc/profile.d/micromamba.sh"
+          else
+              export  PATH="/work/msk/micromamba/bin:$PATH"  # extra space after export prevents interference from conda init
+          fi
+      fi
+      unset __mamba_setup
+      # <<< mamba initialize <<<
+    '';
+  };
+  micromamba-zsh = pkgs.writeTextFile {
+    name = "micromamba.zsh";
+    text = ''
+      # >>> mamba initialize >>>
+      # !! Contents within this block are managed by 'mamba init' !!
+      export MAMBA_EXE="${pkgs.micromamba}/bin/micromamba";
+      export MAMBA_ROOT_PREFIX="/work/msk/micromamba";
+      __mamba_setup="$('${pkgs.micromamba}/bin/micromamba' shell hook --shell zsh --prefix '/work/msk/micromamba' 2> /dev/null)"
       if [ $? -eq 0 ]; then
           eval "$__mamba_setup"
       else
@@ -62,7 +64,7 @@ in
   ];
 
   programs.zsh.initExtra = ''
-    [[ -f ${conda-zsh} ]] && source ${conda-zsh}
+    [[ -f ${micromamba-zsh} ]] && source ${micromamba-zsh}
     [[ -f /etc/profile.d/modules.sh ]] && source /etc/profile.d/modules.sh
   '';
 
