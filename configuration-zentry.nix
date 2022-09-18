c90d45c6926c2dbef2c5ce8be594d401a4d5b464
Michael Schantz Klausen <sch@ntz.nu>
2022-09-18 14:04:37 +0200

    Added plex

--- configuration-zentry.nix
+++ configuration-zentry.nix
@@ -43,11 +43,10 @@
   # Enable the OpenSSH daemon.
   services.openssh.enable = true;
 
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
+  services.plex = {
+    enable = true;
+    openFirewall = true;
+  };
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
