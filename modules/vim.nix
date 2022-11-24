6815fa1291bb81aef118f622e86102aef91b2442
Michael Schantz Klausen <msk@evaxion-biotech.com>
2022-11-24 10:08:27 +0100

    Added snakefmt vim plugin

--- modules/vim.nix
+++ modules/vim.nix
@@ -23,6 +23,50 @@ let
     } + "/misc/vim";
   };
 
+  vim-snakefmt = pkgs.vimUtils.buildVimPlugin rec {
+    name = "vim-snakefmt";
+    pname = name;
+    src = pkgs.fetchFromGitHub {
+      owner = "snakemake";
+      repo = "snakefmt";
+      rev = "v0.7.0";
+      sha256 = "sha256-4kpw89siUWIt1fzDplb04i2BtUR0UNk6MrTYgPdmfY0=";
+    };
+    dontBuild = true;
+  };
+
+  python-importlib-metadata17 = pkgs.python3.pkgs.buildPythonPackage rec {
+    pname = "importlib-metadata";
+    version = "1.7.0";
+    format = "pyproject";
+    src = pkgs.python3.pkgs.fetchPypi {
+      pname = "importlib_metadata";
+      inherit version;
+      hash = "sha256-kLtljNu/bRc1tjQc5wj8cCSj4U6Z/9xXg+3qn5sHf4M=";
+    };
+    nativeBuildInputs = with pkgs.python3.pkgs; [ setuptools setuptools-scm ];
+    propagatedBuildInputs = with pkgs.python3.pkgs; [ toml zipp ];
+    doCheck = false;
+  };
+
+  python-snakefmt = pkgs.python3.pkgs.buildPythonPackage rec {
+    pname = "snakefmt";
+    version = "0.7.0";
+    format = "pyproject";
+    src = pkgs.fetchFromGitHub {
+      owner = "snakemake";
+      repo = "snakefmt";
+      rev = "v0.7.0";
+      sha256 = "sha256-4kpw89siUWIt1fzDplb04i2BtUR0UNk6MrTYgPdmfY0=";
+    };
+    #doCheck = false;
+    nativeBuildInputs = with pkgs.python3.pkgs; [ poetry ];
+    propagatedBuildInputs = with pkgs.python3.pkgs; [
+      black
+      python-importlib-metadata17
+    ];
+  };
+
 in {
   programs.neovim = {
     enable = true;
@@ -31,6 +75,7 @@ in {
     plugins = with pkgs.vimPlugins; [
       vim-nix
       vim-buftabline
+      vim-snakefmt
       {
         plugin = vim-snakemake;
         config = ''
@@ -57,6 +102,8 @@ in {
       }
     ];
 
+    extraPython3Packages = (ps: [ python-snakefmt ]);
+
     extraConfig = ''
       set list
       set listchars=tab:╰─,trail:·
@@ -76,6 +123,7 @@ in {
       nnoremap <A-6> :6b<CR>
 
       au FileType python nnoremap <leader>y :0,$!${pkgs.black}/bin/black - -q<Cr>
+      au FileType snakemake nnoremap <leader>y :Snakefmt<Cr>
     '';
 
   };
