d3a4135ec19607eb11f83377974cf523ba60cb49
Michael Schantz Klausen <sch@ntz.nu>
2022-12-14 09:48:17 +0100

    remove maestral

--- modules/work.nix
+++ modules/work.nix
@@ -21,23 +21,4 @@ in {
     ./syncthing.nix
   ];
 
-  systemd.user.services.maestral = {
-    Unit = {
-      Description = "Maestral GUI";
-      Requires = [ "tray.target" ];
-      After = [ "graphical-session-pre.target" "tray.target" ];
-      PartOf = [ "graphical-session.target" ];
-    };
-
-    Service = {
-      ExecStart = "${pkgs.maestral-gui}/bin/maestral_qt";
-      Restart = "on-failure";
-    };
-
-    Install = { WantedBy = [ "graphical-session.target" ]; };
-  };
-
-  # services.dropbox.enable = true;
-  # services.dropbox.path = "${config.home.homeDirectory}/dropbox";
-
 }
