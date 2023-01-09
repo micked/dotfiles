ee3daed954fd1ac90e09cf9a27d1153e1a89549d
Michael Schantz Klausen <sch@ntz.nu>
2023-01-09 09:46:35 +0100

    Updated helix conf

--- modules/hx.nix
+++ modules/hx.nix
@@ -1,14 +1,39 @@
-{ config, pkgs, libs, ... }:
 {
-  home.packages = with pkgs; [
-    rust-analyzer
-  ];
-
+  config,
+  pkgs,
+  libs,
+  ...
+}: {
   programs.helix = {
     enable = true;
     settings = {
-      theme = "base16";
+      #theme = "base16";
+      editor = {
+        rulers = [80 120];
+        cursor-shape.insert = "bar";
+      };
     };
+    languages = [
+      {
+        name = "rust";
+        language-server = {command = "${pkgs.rust-analyzer}/bin/rust-analyzer";};
+      }
+      {
+        name = "python";
+        language-server = {command = "${pkgs.python310Packages.python-lsp-server}/bin/pylsp";};
+        formatter = {
+          command = "${pkgs.black}/bin/black";
+          args = ["-q" "-"];
+        };
+      }
+      {
+        name = "nix";
+        language-server = {command = "${pkgs.nil}/bin/nil";};
+        formatter = {
+          command = "${pkgs.alejandra}/bin/alejandra";
+          args = ["--quiet"];
+        };
+      }
+    ];
   };
-
 }
