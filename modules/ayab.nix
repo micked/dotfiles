e4e0663e1b1e7f1e782489640a06cd424a0d029f
Michael Schantz Klausen <sch@ntz.nu>
2023-03-25 14:58:21 +0100

    Update AYAB

--- modules/ayab.nix
+++ modules/ayab.nix
@@ -1,51 +1,98 @@
 { config, pkgs, libs, ... }:
 let
   python = pkgs.python39;
-  fbs = python.pkgs.buildPythonPackage rec {
-    pname = "fbs";
-    version = "0.2.8";
+
+  altgraph = python.pkgs.buildPythonPackage rec {
+    pname = "altgraph";
+    version = "0.17.3";
     src = python.pkgs.fetchPypi {
       inherit pname version;
-      sha256 = "sha256-ZKO9Jd7/EJCWVxMBNMGFhLriQPjgN1BQst3TIoUW1Wk=";
+      sha256 = "sha256-rTM1gRTffJQWzbj6HqpYUhZsUFEYcXAhxqjHx6u9A90=";
     };
     doCheck = false;
   };
-  sliplib = python.pkgs.buildPythonPackage rec {
-    pname = "sliplib";
-    version = "0.3.0";
+
+  macholib = python.pkgs.buildPythonPackage rec {
+    pname = "macholib";
+    version = "1.16.2";
     src = python.pkgs.fetchPypi {
       inherit pname version;
-      sha256 = "sha256-3mINsCU1DCas3c72uX5pI0L/rGVET5fa1FTcg2UBdGs=";
+      sha256 = "sha256-VXu/obslXCDpq6/n7WzYBGtI2VJdsvm3fTEipjoqi/g=";
+    };
+    propagatedBuildInputs = with python.pkgs; [ altgraph ];
+    doCheck = false;
+  };
+
+  pyinstaller = python.pkgs.buildPythonPackage rec {
+    pname = "PyInstaller";
+    version = "3.4";
+    src = python.pkgs.fetchPypi {
+      inherit pname version;
+      sha256 = "sha256-pabgSmar/Ph2Homi662TeRnGvjOnuJY+GpYbVcs1mGs=";
+    };
+    propagatedBuildInputs = with python.pkgs; [
+      pefile
+      altgraph
+      macholib
+    ];
+    doCheck = false;
+  };
+
+  fbs = python.pkgs.buildPythonPackage rec {
+    pname = "fbs";
+    version = "0.8.6";
+    src = python.pkgs.fetchPypi {
+      inherit pname version;
+      sha256 = "sha256-hIGRJlAtlZBQJ8jGp4j5PzGPHADsFk3eXvBtup0ROc8=";
+    };
+    propagatedBuildInputs = with python.pkgs; [
+      pyinstaller
+    ];
+    doCheck = false;
+  };
+
+  sliplib = python.pkgs.buildPythonPackage rec {
+    pname = "sliplib";
+    version = "0.4.0";
+    src = python.pkgs.fetchPypi {
+      inherit pname version;
+      sha256 = "sha256-n1Fc6ZOzR6aaTqeEEQsaVoS/h1JT/mrP8ielBzn2Pk4=";
     };
     buildInputs = with python.pkgs; [ sphinx_rtd_theme ];
     doCheck = false;
   };
-  fysom = python.pkgs.buildPythonPackage rec {
-    pname = "fysom";
-    version = "2.1.3";
-    src = python.pkgs.fetchPypi {
-      inherit pname version;
-      sha256 = "sha256-7fRMU+OVdH2vPMRViiX9Ed663yIBbkPr1d9xh7P76Ig=";
-    };
-    doCheck = false;
-  };
+
+  #fysom = python.pkgs.buildPythonPackage rec {
+  #  pname = "fysom";
+  #  version = "2.1.3";
+  #  src = python.pkgs.fetchPypi {
+  #    inherit pname version;
+  #    sha256 = "sha256-7fRMU+OVdH2vPMRViiX9Ed663yIBbkPr1d9xh7P76Ig=";
+  #  };
+  #  doCheck = false;
+  #};
+
   ayab-python = python.withPackages(ps: with ps; [
     pillow
     pyserial
     sliplib
-    fysom
+    #fysom
     playsound
     fbs
     pyqt5
-
+    numpy
+    requests
+    #wave
+    bitarray
+    simpleaudio
   ]);
   ayab-desktop = pkgs.stdenv.mkDerivation {
     name = "ayab-desktop";
     src = pkgs.fetchFromGitHub {
       owner = "AllYarnsAreBeautiful";
       repo = "ayab-desktop";
-      rev = "ab69af7";
-      sha256 = "sha256-m7QvFz3pioTcHUREtvlAKTR0c7p9xe8UhmUydBW7fE0=";
+      rev = "e6101bc";
+      sha256 = "sha256-wg4fm1fEZvE1d1rmy2BxOkDW4v23mH1uT5PXtbJo8Kg=";
     };
 
     buildInputs = [ ayab-python ];
@@ -55,6 +102,19 @@ let
       mkdir -p $out
       cp -r * $out/
       rm $out/bin/ayab
+
+      cd $out
+      pyuic5 src/main/python/ayab/about_gui.ui -o src/main/python/ayab/about_gui.py
+      pyuic5 src/main/python/ayab/firmware_flash_gui.ui -o src/main/python/ayab/firmware_flash_gui.py
+      pyuic5 src/main/python/ayab/main_gui.ui -o src/main/python/ayab/main_gui.py
+      pyuic5 src/main/python/ayab/menu_gui.ui -o src/main/python/ayab/menu_gui.py
+      pyuic5 src/main/python/ayab/mirrors_gui.ui -o src/main/python/ayab/mirrors_gui.py
+      pyuic5 src/main/python/ayab/prefs_gui.ui -o src/main/python/ayab/prefs_gui.py
+      pyuic5 src/main/python/ayab/engine/dock_gui.ui -o src/main/python/ayab/engine/dock_gui.py
+      pyuic5 src/main/python/ayab/engine/options_gui.ui -o src/main/python/ayab/engine/options_gui.py
+      pyuic5 src/main/python/ayab/engine/status_gui.ui -o src/main/python/ayab/engine/status_gui.py
+      pyrcc5 src/main/python/ayab/ayab_logo_rc.qrc -o src/main/python/ayab/ayab_logo_rc.py
+
       substituteInPlace $out/src/main/python/ayab/ayab.py \
         --replace "logging.basicConfig(filename='ayab_log.txt'," "logging.basicConfig(filename='/dev/null',"
 
