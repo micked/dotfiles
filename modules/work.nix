896b34cf49838f7187c77c105f85609a83cc2d46
Michael Schantz Klausen <msk@evaxion.ai>
2026-05-20 14:37:07 +0200

    pymol git

--- modules/work.nix
+++ modules/work.nix
@@ -1,13 +1,20 @@
-{
-  pkgs,
-  ...
-}: let
+{pkgs, ...}: let
   clc = pkgs.writeShellScriptBin "clc" "/home/msk/keep/clc21/clcmainwb21";
+  pymol-git = pkgs.pymol.overrideAttrs (oldAttrs: {
+    src = pkgs.fetchFromGitHub {
+      owner = "schrodinger";
+      repo = "pymol-open-source";
+      rev = "46530027e68dcda561c2126c8e65be8e3102ccc5";
+      hash = "sha256-6+Uuk4qWd/I86I4gCPm8G+1uuozKaB5WMBRtMWC0amE=";
+    };
+    patches = [(builtins.elemAt oldAttrs.patches 0)];
+    disabledTests = oldAttrs.disabledTests ++ ["test_protonate_fallback" "test_protonate_fallback_low_pH"];
+  });
 in {
   home.packages = with pkgs; [
     zotero
     python3Packages.blinkstick
-    pymol
+    pymol-git
     kicad
 
     clc
